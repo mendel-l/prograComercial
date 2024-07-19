873828decd5c6d9ead0739bb0a9949cd04553f77
Arturo Monterroso <110266239+arturomonterroso25@users.noreply.github.com>
2024-07-19 12:00:50 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
-const { Router } = require('express');
-const router = Router();
+# Branching
 
-module.exports = (app) => {
-    app.use('/', router);
-};
+## Descripción
+
+Este proyecto tiene como objetivo enseñarles a utilizar de manera sencilla el branching, los code reviews y las pull request. 
+
+## Nombre de Arturo
+Arturo Monterroso - 201608006
